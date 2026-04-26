fa393f02e17dba3c25d4276b63b92be80cd3cb8a
ralvarezsagardoy <ricardooalvsag2006@gmail.com>
2026-04-26 02:36:53 +0200

    creación del servidor

--- MySmartGrid/src/main/java/server/Servidor.java
+++ MySmartGrid/src/main/java/server/Servidor.java
@@ -1,5 +1,34 @@
 package server;
 
-public class Servidor {
+import java.io.IOException;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import pcd.util.Ventana;
+import servicio.MonitorizacionServicio;
+import servicio.PreciosServicio;
+
+public class Servidor {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		//creamos una ventana para cada servidor
+		Ventana v1 = new Ventana(30, 30, 500, 400, "Servidor Monitorización MySmartGrid gRPC- Puerto 9002");
+        Ventana v2 = new Ventana(550, 30, 500, 400, "Servidor Precios MySmartGrid gRPC- Puerto 9004");    	
+        
+      
+        //configuramos los servidores 
+    	Server serverMonitorizacion = ServerBuilder.forPort(9002).addService(new MonitorizacionServicio(v)).build();
+        Server serverPrecios = ServerBuilder.forPort(9004).addService(new PreciosServicio(v)).build();
+
+        //arrancamos los servidores
+        serverMonitorizacion.start();
+        v1.traza(">>> Servidor Escuchando en puerto 9002...", Ventana.VERDE);
+        
+        serverPrecios.start();
+        v2.traza(">>> Servidor Escuchando en puerto 9004...", Ventana.VERDE);
+       
+        //mantenemos los servidores en ejecución
+        serverMonitorizacion.awaitTermination();
+        serverPrecios.awaitTermination();
+    }
 }
